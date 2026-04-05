a5219e51a4a8f0c9a848c61d530f9249c27b41cc
wsbjwjt <wsbjwjt@gmail.com>
2026-04-05 15:05:10 +0800

    debug: remove Prisma adapter to test OAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,9 @@
 import NextAuth from 'next-auth'
-import { PrismaAdapter } from '@auth/prisma-adapter'
 import GitHub from 'next-auth/providers/github'
 import Google from 'next-auth/providers/google'
-import { prisma } from './prisma'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   secret: process.env.AUTH_SECRET,
-  adapter: PrismaAdapter(prisma),
   providers: [
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID!,
